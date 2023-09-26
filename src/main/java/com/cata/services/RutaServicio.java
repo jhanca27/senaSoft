59bb5f7ae4d93a98872f28314a2da325be819f82
jhanca <jhancarlohigueracarvajal@gmail.com>
2023-09-26 14:14:20 -0500

    "solucion llenado matriz inicial"

--- src/main/java/com/cata/services/RutaServicio.java
+++ src/main/java/com/cata/services/RutaServicio.java
@@ -2,7 +2,6 @@ package com.cata.services;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.cata.models.Conexion;
@@ -13,45 +12,61 @@ import com.cata.models.Ubicacion;
 @Service
 public class RutaServicio {
 	
-	@Autowired
 	private UbicacionService ubicacionService;
 	
-	@Autowired
 	private ConexionService conexionService;
 	
 	
-	int tamanoUbicacion = ubicacionService.contarUbicaciones();
-	int tamanoPesos = conexionService.contarConexiones();
-	int matrizPeso[][] = new int[tamanoUbicacion][tamanoUbicacion]; //matriz adyacencia
-	List<Ubicacion> listaNodos = ubicacionService.encontrarTodasUbicaciones();  // arreglo nodos
-	List<String> listaNombreNodos = ubicacionService.encontrarListaNombreNodo();
+	public RutaServicio(UbicacionService ubicacionService, ConexionService conexionService) {
+		this.ubicacionService = ubicacionService;
+		this.conexionService = conexionService;
+	}
 	
-	public void llenadoMatriz() {
+	
+	
+	
+	public int[][] llenadoMatriz() {
+		int tamanoUbicacion = ubicacionService.contarUbicaciones();
+		int tamanoPesos = conexionService.contarConexiones();
+		int matrizPeso[][] = new int[tamanoUbicacion][tamanoUbicacion]; //matriz adyacencia
+		List<Ubicacion> listaNodos = ubicacionService.encontrarTodasUbicaciones();
 		
-		for(int i=0; i<tamanoUbicacion; i++) {
-			for(int j=0 ; j<tamanoUbicacion;i++) {
+		List<String> listaNombreNodos = ubicacionService.encontrarListaNombreNodo();
+		
+		for(int i=0; i < tamanoUbicacion; i++) {
+			for(int j=0 ; j < tamanoUbicacion;j++) {
+				Ubicacion ubicacionActual = ubicacionService.encontrarUbicacionNombre(listaNombreNodos.get(i));
 				if(i == j) {
 					matrizPeso[i][j] = 0;
 				}else {
-					matrizPeso[i][j] = match(listaNodos,listaNombreNodos.get(j));
+					if(matrizPeso[i][j] == 0) {
+						matrizPeso[i][j] = match(ubicacionActual,listaNombreNodos.get(j),listaNombreNodos.get(i));
+						matrizPeso[j][i] = matrizPeso[i][j] ;
+					}
+						
+					
 				}
 				
 			}
+			
 		}
+		return matrizPeso;
 		
 	}
 	
-	public int match(List<Ubicacion> listaUbicacion,String nodo) {
-		for(Ubicacion ubicacion:listaUbicacion) {
-			List<Conexion> listaConexiones = conexionService.encontrarConexionUbicacionO(ubicacion);
+	public int match(Ubicacion ubicacionActual,String nodoO,String nodoT) {
+			List<Conexion> listaConexiones = conexionService.encontrarConexionUbicacionO(ubicacionActual);
 			for(Conexion conexion:listaConexiones) {
-				if(conexion.getUbicacionT().getNombre() == nodo ) {
+				if(conexion.getUbicacionT().getNombre() == nodoO ) {
 					return conexion.getPeso();
 				}
 			}	
-		}
+		
 		return 0;
 		
 	}
+
 		
+	
+	
 }
