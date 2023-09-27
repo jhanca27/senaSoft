27fa5a3f76313d45e86bd4b8d2bece8838138f5c
jhanca <jhancarlohigueracarvajal@gmail.com>
2023-09-27 12:38:24 -0500

    "servicio ruta casi completado"

--- src/main/java/com/cata/services/RutaServicio.java
+++ src/main/java/com/cata/services/RutaServicio.java
@@ -1,7 +1,12 @@
 package com.cata.services;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
+import org.springframework.data.util.Predicates;
 import org.springframework.stereotype.Service;
 
 import com.cata.models.Conexion;
@@ -27,9 +32,7 @@ public class RutaServicio {
 	
 	public int[][] llenadoMatriz() {
 		int tamanoUbicacion = ubicacionService.contarUbicaciones();
-		int tamanoPesos = conexionService.contarConexiones();
 		int matrizPeso[][] = new int[tamanoUbicacion][tamanoUbicacion]; //matriz adyacencia
-		List<Ubicacion> listaNodos = ubicacionService.encontrarTodasUbicaciones();
 		
 		List<String> listaNombreNodos = ubicacionService.encontrarListaNombreNodo();
 		
@@ -62,9 +65,78 @@ public class RutaServicio {
 				}
 			}	
 		
-		return 0;
+		return Integer.MAX_VALUE;
 		
 	}
+	
+	
+	public void DijkstraAlgorithm(int[][] matriz, int source, int destination) {
+			
+		int n = matriz.length;
+		int[] distancias = new int[n];
+		Integer[] rutas = new Integer[n];
+		int contador = 0;
+		int[][] matrizOperacion = matriz;
+		List<Integer> listaPesos = new ArrayList<>();
+		List<List<Integer>> listaRuta = new ArrayList<>();
+		int refi = 0;
+		int refj = 0;
+		int pesoTotal = 0;
+		
+		
+		for(int i=source; i < n; i++) {
+			int minDistance = Integer.MAX_VALUE;
+			for(int j = 0 ; j < n ;j++) {
+				if(refi == j && refj ==i) {
+					 continue;
+				  }
+				if(matrizOperacion[i][j] != 0 && matrizOperacion[i][j] < minDistance ) {
+				  distancias[contador] = matriz[i][j];
+				  rutas[contador] = j;
+				  contador += 1;
+				  refi = i;
+				  refj = j;
+				  i = j-1;
+				  
+				 
+				  if(destination-1 == j) {
+					  matrizOperacion[refi][refj] = Integer.MAX_VALUE; 
+					  
+					  //listaPesos.add(Arrays.asList(distancias).stream().mapToInt(Integer::intValue).sum());
+					  pesoTotal = Arrays.stream(distancias).parallel().reduce(0,(a,b)->  a + b);
+					  listaPesos.add(pesoTotal);
+					  List<Integer> lista = new ArrayList<Integer>(Arrays.asList(rutas));
+					  while (lista.remove(null)) {}
+					  listaRuta.add(lista);	
+					  distancias = new int[n];
+					  rutas = new Integer[n];
+					  
+					  i = source;
+					  j = -1;
+					  contador = 0;
+					  break;
+				  }
+				  
+				  break;
+				}
+				if(i == 0 && j == destination-1) {
+					i=n;
+				}
+				if(i!=0 && i!= n && j == n-1){
+					matriz[refi][refj] = Integer.MAX_VALUE;
+					i = source;
+					contador=0;
+				}
+			}		
+		}
+		
+		System.out.print(listaPesos);
+		System.out.print(listaRuta);
+		
+	}
+	
+	
+	
 
 		
 	
